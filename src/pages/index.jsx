85e1911572d13463189f71d29bb7e58117cf85d9
Ruan Manoel <ruuanmanoel@gmail.com>
2024-09-28 17:51:29 -0300

    :tada: Iniciando a estrutura do userhubreact

---
+++ src/pages/index.jsx
@@ -0,0 +1,14 @@
+function Cadastro(){
+    return(
+        <div>
+            <h2>Cadastro</h2>
+            <form action="">
+                <input placeholder="Nome" type="text" required/>
+                <input placeholder="Nome" type="email" required/>
+                <input placeholder="Nome" type="password" required/>
+            </form>
+        </div>
+    )
+}
+
+export default Cadastro
