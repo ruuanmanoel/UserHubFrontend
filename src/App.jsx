68357103ee50ac798ac9cf2f522f2c15b753086c
Ruan Manoel <ruuanmanoel@gmail.com>
2024-09-28 18:57:21 -0300

    :sparkles: feat: importando pagina de cadastro e login

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,17 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom"
+import Cadastro from "./pages/cadastro"
+import Login from "./pages/login"
+
 function App() {
 
 
   return (
-    <>
-      <h1>Cadastro</h1>
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Cadastro/>}/>
+        <Route path="/login" element={<Login/>}/>
+      </Routes>
+    </BrowserRouter>
   )
 }
 
